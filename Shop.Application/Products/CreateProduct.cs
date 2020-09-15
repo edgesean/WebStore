91258a21caff11042369c031c08086853a0d7b28
edgesean <43985841+edgesean@users.noreply.github.com>
2020-09-15 12:25:05 +0200

    Initial Commit
    Application and database setups, linking of application, database, domain and UI

---
+++ Shop.Application/Products/CreateProduct.cs
@@ -0,0 +1,34 @@
+﻿using Shop.Database;
+using Shop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Shop.Application.Products
+{
+    public class CreateProduct
+    {
+        private ApplicationDbContext _context;
+
+        public CreateProduct(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+
+        public void Do(int id, string Name, string Description)
+        {
+            _context.Products.Add(new Product
+            {
+                Id = id,
+                Name = Name,
+                Description = Description
+            });
+                
+        }
+
+    }
+}
